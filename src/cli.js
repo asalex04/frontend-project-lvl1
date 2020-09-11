02400375f8262a1d019b54f2911994ba600457ea
asalex04 <arc04@mail.ru>
2020-09-11 13:20:39 +0400

    made brain-even

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export function getUserName() {
+export default function getUserName() {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
-}
-
-export function getUserAnswer() {
-  const answer = readlineSync.question('You answer: ');
-  return answer;
 }
